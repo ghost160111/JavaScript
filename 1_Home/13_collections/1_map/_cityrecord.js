45255a01e03887bbad6970c146480e31613fa71b
ghost160111 <jalol100.ghost@yandex.com>
2023-05-24 14:53:51 +0500

    .

--- 1_Home/13_collections/1_map/_cityrecord.js
+++ 1_Home/13_collections/1_map/_cityrecord.js
@@ -34,32 +34,22 @@ citiesMap
     .set("MSC", new CityRecord("Massachussets", 10433452, 656.33, 324.34));
 
 const getCityData = (cityKey) => {
-    if (citiesMap.keys(cityKey) !== undefined || null) {
+    if (citiesMap.keys(cityKey) !== undefined || null)
         console.log(citiesMap.get(cityKey).iterate());
-    } else {
+    else
         console.log('There is no city like this: "' + cityKey + '"');
-    }
 }
 
 const getAllCitiesList = () => {
-    for (const city of cities) {
+    for (const city of citiesMap.values()) {
         console.log(city.iterate());
     }
 }
 
-const getAllCitiesList2ndVersion = () => cities.forEach(city => console.log(city.iterate()));
+const getAllCitiesList2ndVersion = () => citiesMap.forEach(city => console.log(city.iterate()));
 
-// EXAMPLE: GET SPECIFIC CITY USING ITS KEY
-getCityData("NYC");
-// City: New York
-// Population: 14433300
-// Latitude: 214.42
-// Altitude: 232.32
-
-// EXAMPLE: GET ALL DATA ABOUT ALL CITIES
-getAllCitiesList();
-// Output: prints the data about all cities
-getAllCitiesList2ndVersion();
-// Output: prints the data about all cities
+//getCityData("NYC");           // Gets data about city by specific key entered
+//getAllCitiesList();           // Gets data about whole record
+//getAllCitiesList2ndVersion(); // Gets data about whole record 2nd version
 
 //#endregion
