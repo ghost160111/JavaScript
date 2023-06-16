319d139973030e5571c330687816c9526def4ccd
ghost160111 <jalol100.ghost@yandex.com>
2023-06-16 15:03:47 +0500

    Added find.js file

--- 3_Advanced/1_JS_array_methods/03_finding_elements/3_find/find.js
+++ 3_Advanced/1_JS_array_methods/03_finding_elements/3_find/find.js
@@ -1,3 +1,13 @@
 let num = [1,2,3,4,5];
 let evenNumCount = num.find(qty => qty % 2 === 0);
-console.log(evenList); // 2
+console.log(evenNumCount); // 2
+
+let customers = [
+    { name: "ABC Inc", credit: 100 },
+    { name: "ACME Corp", credit: 200 },
+    { name: "IoT AG", credit: 300 },
+];
+
+let check = customers.find(c => c.credit > 100);
+
+console.log(check); // { name: 'ACME Corp', credit: 200 }
